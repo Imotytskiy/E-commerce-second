5b4ea460d3b071635336f4e066b095ea4c1942da
Igor Motytskiy <imotytskiy@gmail.com>
2024-08-23 14:27:12 +0200

    add

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,18 @@
+import logo from "./assets/logo_forbest.png";
+
+export const assets = {
+  logo,
+};
+
+export const products = {
+  _id: "aaaaaa",
+  name: "Girls Roound neck Cotton body",
+  description: "Very good for kid",
+  price: 100,
+  image: [p_img1],
+  category: "Kids",
+  subCategory: "Topwear",
+  sizes: ["S", "M", "L"],
+  date: 171663767234,
+  bestseller: true,
+};
