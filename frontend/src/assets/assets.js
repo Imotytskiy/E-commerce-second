02c37acf3bb3892b375793b0ba109898c9d05189
Igor Motytskiy <imotytskiy@gmail.com>
2024-08-24 12:27:51 +0200

    commite navbar

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,17 @@
-import logo from "./assets/logo_forbest.png";
+import logo_icon from "./logo.png";
+import search_icon from "./search_icon.png";
+import profile_icon from "./profile_icon.png";
+import cart_icon from "./cart_icon.png";
+import menu_icon from "./menu_icon.png";
+import back_icon from "./back_icon.png";
 
 export const assets = {
-  logo,
+  logo_icon,
+  search_icon,
+  profile_icon,
+  cart_icon,
+  menu_icon,
+  back_icon,
 };
 
 export const products = {
@@ -9,7 +19,7 @@ export const products = {
   name: "Girls Roound neck Cotton body",
   description: "Very good for kid",
   price: 100,
-  image: [p_img1],
+  // image: [p_img1],
   category: "Kids",
   subCategory: "Topwear",
   sizes: ["S", "M", "L"],
