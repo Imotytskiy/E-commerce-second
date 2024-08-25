0d065b7067208093d4d499791a9274425edc719b
Igor Motytskiy <imotytskiy@gmail.com>
2024-08-25 20:59:33 +0200

    add

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -21,9 +21,9 @@ const BestSeller = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {bestSeller.map((item) => (
+        {bestSeller.map((item, index) => (
           <ProducItem
-            key={item._id}
+            key={index}
             id={item._id}
             name={item.name}
             image={item.image}
