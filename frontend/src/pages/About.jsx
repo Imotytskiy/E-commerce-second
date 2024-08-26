0de208ed9241d07e6aee5c20350d9b60a8dfa4da
Igor Motytskiy <imotytskiy@gmail.com>
2024-08-27 00:31:52 +0200

    add update

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+
+const About = () => {
+  return (
+    <>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img className="w-full md:max-w-[450px]" src={assets.about.img} />
+      </div>
+    </>
+  );
+};
+
+export default About;
